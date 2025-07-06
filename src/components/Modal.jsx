11422b982f49ac36afbf11c2d7d3679722d27b25
Salvatore <workstudioms@gmail.com>
2025-07-06 14:33:25 +0200

    feat: Milestone 9 complete

---
+++ src/components/Modal.jsx
@@ -0,0 +1,26 @@
+// Hooks
+import ReactDOM from 'react-dom';
+
+function Modal(props) {
+
+    // passaggio delle proprietà (props) in maniera ordinata
+    const { title, content, show = false, onClose = () => { }, onConfirm = () => { }, confirmText = "Conferma" } = props;
+
+
+    return show && ReactDOM.createPortal(
+        <div className="modal-container">
+            <div className="modal">
+                <h2>{title}</h2>
+                <p>{content}</p>
+                <button onClick={onClose}>Annulla</button>
+                <button onClick={onConfirm}>{confirmText}</button>
+            </div>
+        </div>,
+        // document.body inserisce il contenuto in <body>
+        // ma fuori dal normale flusso di rendering React
+        document.body
+    )
+
+}
+
+export default Modal;
--- src/pages/TaskDetails.jsx
+++ src/pages/TaskDetails.jsx
@@ -1,14 +1,19 @@
 // Hooks
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
-import GlobalContext from "../context/GlobalContext";
 import { useNavigate } from "react-router-dom";
+
+// Componenti
+import GlobalContext from "../context/GlobalContext";
+import Modal from "../components/Modal";
 
 function TaskDetails() {
 
     const { id } = useParams();
     const { tasks, removeTask } = useContext(GlobalContext);
     const navigate = useNavigate()
+
+    const [showModal, setShowModal] = useState(false);
 
     // Trova l'id della task
     const task = tasks.find(task => task.id.toString() === id);
@@ -29,7 +34,15 @@ function TaskDetails() {
             <span>Descrizione: {task.description}</span>
             <span>Stato: {task.status}</span>
             <span>Data di creazione: {new Date(task.createdAt).toLocaleDateString()}</span>
-            <button onClick={handleDelete}>Elimina la task</button>
+            <button onClick={() => setShowModal(true)}>Elimina la task</button>
+            <Modal
+                show={showModal}
+                title="Conferma Eliminazione"
+                content={`Sei sicuro di voler eliminare la task "${task.title}"?`}
+                onClose={() => setShowModal(false)}
+                onConfirm={handleDelete}
+                confirmText="Elimina"
+            />
         </div>
     );
 }
